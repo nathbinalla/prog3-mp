85b3115c28fb165a5a2c1ca0eb93aeceddd9f4ab
nathbinalla <89829147+nathbinalla@users.noreply.github.com>
2021-08-31 13:47:23 +0800

    Update Player.java

--- Player.java
+++ Player.java
@@ -36,6 +36,16 @@ class Player {
 	public int getNumber() {
 		return number;
 	}
+	
+	/** This method makes the player draw a piece from the class Bag
+
+    	@param b the bag where the player draws a piece from
+    	@return a random piece from the bag
+	*/
+	public Piece drawPiece(Bag b)
+	{
+		return b.getPiece();
+	}
 
 	private Piece[] pieces;
 	private Tile[] denTiles;
